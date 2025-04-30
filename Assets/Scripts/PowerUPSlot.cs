3384d4f02b238ebcb307937c9411da5d5b49af43
SebnemGunduz <sebnemgunduz0@gmail.com>
2025-04-30 13:52:16 +0300

    PlaneReadyPart

--- Assets/Scripts/PowerUPSlot.cs
+++ Assets/Scripts/PowerUPSlot.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PowerUPSlot : MonoBehaviour
+public class PowerUpSlot : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Slider ve Uçak Ayarlarý")]
+    public Slider powerSlider; 
+    public GameObject[] planes; 
+
+    private bool[] slotActivated;
+
     void Start()
     {
-        
+        if (planes.Length != 8)
+        {
+            Debug.LogError("Lütfen tam olarak 8 uçak tanýmlayýn.");
+            return;
+        }
+
+        slotActivated = new bool[planes.Length];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float currentValue = powerSlider.value; 
+        float slotStep = 1f / planes.Length;
+
+        for (int i = 0; i < planes.Length; i++)
+        {
+            if (!slotActivated[i] && currentValue >= slotStep * (i + 1))
+            {
+                slotActivated[i] = true;
+
+                
+                Debug.Log($"Uçak {i + 1} kalkýþa hazýr.");
+
+                
+                
+            }
+        }
+    }
+
+    public void ResetSlots()
+    {
+        for (int i = 0; i < planes.Length; i++)
+        {
+            slotActivated[i] = false;
+        }
     }
 }
